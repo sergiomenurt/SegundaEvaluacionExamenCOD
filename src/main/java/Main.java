f99e7d66d3dee8155aaad5a8e3826159310826fe
Sergio Mendez Urtaza <smendezurtaza@danielcastelao.org>
2022-03-16 14:50:27 +0100

    Commit codigo completo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
 public class Main {
+
+    public static void main(final String[] args) {
+        final String token = args[0]; // el token del bot no lo pude conseguir debido al gran numero de personas con la misma IP que entramos al mismo sitio
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
+    }
+}
+
 }
